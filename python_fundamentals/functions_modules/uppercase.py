ee53087cbcdb99a8377474da8274581ec485f362
14835-create <14835@holbertonstudents.com>
2026-09-21 09:29:15 +0200

    error fix

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -7,4 +7,7 @@ def uppercase(str):
             result += chr(ord(c) - 32)
         else:
             result += c
-            print("{}".format(result))
+    print("{}".format(result))
+
+
+uppercase("Holberton")
