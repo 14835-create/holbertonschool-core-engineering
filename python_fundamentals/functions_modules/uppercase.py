a9b1784869b6311b09b5a2e3f32cea8778986441
14835-create <14835@holbertonstudents.com>
2026-09-21 09:18:23 +0200

    uppercase

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -2,9 +2,9 @@
 
 def uppercase(str):
     result = ""
-    for c in s:
+    for c in str:
         if 97 <= ord(c) <= 122:
             result += chr(ord(c) - 32)
         else:
             result += c
-            print(result)
+            print("{}".format(result))
