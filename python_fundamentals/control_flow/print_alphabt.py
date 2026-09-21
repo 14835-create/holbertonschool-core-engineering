6adeba5ce5d19425a95b403ba3fc4ede3003c8b5
14835-create <14835@holbertonstudents.com>
2026-09-21 06:01:15 +0200

    newline

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python3
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-print("{}".format("".join(c for c in alphabet if c not in ("q", "e"))))
+print("{}".format("".join(c for c in alphabet if c not in ("q", "e"))), end ="")
