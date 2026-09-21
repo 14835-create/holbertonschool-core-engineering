ec639bfa643b639c9749782b388bd9c0277d574b
14835-create <14835@holbertonstudents.com>
2026-09-21 05:53:13 +0200

    error

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-print("".join(c for c in alphabet if c not in ("q", "e")))
+print("{}".format("".join(c for c in alphabet if c not in ("q", "e"))))
