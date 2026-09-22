a97793fb626ed46c95a20abb1d75078006b0cd65
14835-create <14835@holbertonstudents.com>
2026-09-22 02:40:18 +0200

    :C

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 def pow(a, b):
+    a = int(a)
     b = int(b)
+
     result = 1
     for _ in range(b):
         result *= a
-        print(result, end="")
+
         return result
