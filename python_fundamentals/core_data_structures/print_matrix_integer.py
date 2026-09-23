4c481b36dc2ed4b8610dccbed5060d75f71a0aed
14835-create <14835@holbertonstudents.com>
2026-09-23 07:01:07 +0200

    print matrix

---
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, value in enumerate(row):
+            if i != 0:
+                print(" ", end="")
+            print("{:d}".format(value), end="")
+        print()
